e79080a00486e5f69044dbe58eaf9d1cd4616416
Arthur Danjou <arthurdanjou@outlook.fr>
2021-06-18 21:42:13 +0200

    Working

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -13,6 +13,18 @@ export default class AppProvider {
   }
 
   public async ready () {
+    const { default: User } = await import('App/Models/User')
+    const { default: Env } = await import('@ioc:Adonis/Core/Env')
+    const { default: Logger } = await import('@ioc:Adonis/Core/Logger')
+    const user = await User.firstOrCreate({
+      email: Env.get('ADMIN_USER', 'admin@linkyjs.dev')
+    }, {
+      email: Env.get('ADMIN_USER', 'admin@linkyjs.dev'),
+      password: Env.get('ADMIN_PASSWORD', 'password')
+    })
+    if (user) {
+      Logger.info('User successfully created')
+    }
   }
 
   public async shutdown () {
