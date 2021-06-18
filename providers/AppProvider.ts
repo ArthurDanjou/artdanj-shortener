a43deb268d663e826a96f494b72d5cd611f6a371
Arthur Danjou <arthurdanjou@outlook.fr>
2021-06-18 22:00:47 +0200

    Working

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -13,30 +13,29 @@ export default class AppProvider {
   }
 
   public async ready () {
-    const { default: User } = await import('App/Models/User')
-    const { default: Env } = await import('@ioc:Adonis/Core/Env')
     const { default: Logger } = await import('@ioc:Adonis/Core/Logger')
-    const user = await User.firstOrCreate({
-      email: Env.get('ADMIN_USER', 'admin@linkyjs.dev')
-    }, {
-      email: Env.get('ADMIN_USER', 'admin@linkyjs.dev'),
-      password: Env.get('ADMIN_PASSWORD', 'password')
-    })
-    if (user) {
-      Logger.info('Admin User successfully created !')
-    }
-
     const { default: Migrator } = await import('@ioc:Adonis/Lucid/Migrator')
     const { default: Database } = await import('@ioc:Adonis/Lucid/Database')
     const { default: Application } = await import('@ioc:Adonis/Core/Application')
+
     const migrator = new Migrator(Database, Application, {
       direction: 'up',
     })
 
     await migrator.run()
-
     if (migrator.status === 'completed') {
       Logger.info('Migrations successfully passed !')
+      const { default: User } = await import('App/Models/User')
+      const { default: Env } = await import('@ioc:Adonis/Core/Env')
+      const user = await User.firstOrCreate({
+        email: Env.get('ADMIN_USER', 'admin@linkyjs.dev')
+      }, {
+        email: Env.get('ADMIN_USER', 'admin@linkyjs.dev'),
+        password: Env.get('ADMIN_PASSWORD', 'password')
+      })
+      if (user) {
+        Logger.info('Admin User successfully created !')
+      }
     }
   }
 
